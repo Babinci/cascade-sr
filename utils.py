3d661a46902efd69e0f780024ef92353dcda7ac4
babinci <walkowo@gmail.com>
2021-08-30 18:54:37 +0200

    added function for loading media path

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
 import torch
+import os
+print(torch)
+
+
+def load_images_list(directory, ends='png'):
+    img_list = [
+        os.path.join(root,name)
+        for root, dirs,files in os.walk(directory, topdown=False)
+        for name in files
+        if os.path.join(root, name).endswith(ends)
+    ]
+    return img_list
