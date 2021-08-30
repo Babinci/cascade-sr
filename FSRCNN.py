ea8d17efb0b37f4f9d79f76290566e4c08fcce11
babinci <walkowo@gmail.com>
2021-08-30 18:42:06 +0200

    first cascade version of network

--- FSRCNN.py
+++ FSRCNN.py
@@ -1,26 +1,58 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
+
+
 
 class FSRCNN(nn.Module):
-    def __init__(self, d=56, s=12, m=4):
+    def __init__(self, scale_factor=2, num_channels=3, d=56, s=12, m=4):
         super(FSRCNN, self).__init__()
+        self.first_part = nn.Sequential(
+            nn.Conv2d(num_channels, d, kernel_size=5, padding=5//2),
+            nn.PReLU(d)
+        )
+        self.mid_part = [nn.Conv2d(d, s, kernel_size=1), nn.PReLU(s)]
+        for _ in range(m):
+            self.mid_part.extend([nn.Conv2d(s, s, kernel_size=3, padding=3//2), nn.PReLU(s)])
+        self.mid_part.extend([nn.Conv2d(s, d, kernel_size=1), nn.PReLU(d)])
+        self.mid_part = nn.Sequential(*self.mid_part)
+        self.last_part = nn.ConvTranspose2d(d, num_channels, kernel_size=9, stride=scale_factor, padding=9//2,
+                                            output_padding=scale_factor-1)
 
-        self.m = m
+        self._initialize_weights()
 
-        self.feature= nn.Conv2d(1,d, 5)
-        self.shrink=nn.Conv2d(d,s,1)
-        self.map = nn.Conv2d(s,s,3)
-        self.expand = nn.Conv2d(s,d,1)
-        self.deconv= nn.ConvTranspose2d(s,1,9)
-
+    def _initialize_weights(self):
+        for m in self.first_part:
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight.data, mean=0.0, std=math.sqrt(2/(m.out_channels*m.weight.data[0][0].numel())))
+                nn.init.zeros_(m.bias.data)
+        for m in self.mid_part:
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight.data, mean=0.0, std=math.sqrt(2/(m.out_channels*m.weight.data[0][0].numel())))
+                nn.init.zeros_(m.bias.data)
+        nn.init.normal_(self.last_part.weight.data, mean=0.0, std=0.001)
+        nn.init.zeros_(self.last_part.bias.data)
 
     def forward(self, x):
-
-        x = F.prelu(self.feature(x))
-        x = F.prelu(self.shrink(x))
-        for i in range(self.m):
-            x = F.prelu(self.map(x))
-        x = F.prelu(self.expand(x))
-        x = F.prelu(self.deconv(x))
+        x = self.first_part(x)
+        x = self.mid_part(x)
+        x = self.last_part(x)
         return x
+
+
+class Cascade(nn.Module):
+    def __init__(self):
+        super(Cascade, self).__init__()
+        self.first = FSRCNN()
+        self.second = FSRCNN()
+        self.third = FSRCNN()
+
+    def forward(self,x):
+        x = self.first(x)
+        x = self.second(x)
+        x = self.third(x)
+
+        return x
+
+    
