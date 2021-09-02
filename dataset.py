f2459dc3500583d0eb509821f5925233cfb05ae5
Babinci <walkowo@gmail.com>
2021-09-02 18:03:45 +0200

    first dataloader for dummy fsrcnn training

--- dataset.py
+++ dataset.py
@@ -2,6 +2,11 @@ from utils import *
 from torch.utils.data import Dataset
 import torchvision
 from torch.utils.data import DataLoader
+import torch.nn.functional as F 
+
+
+def resize_img(img, scale =.5):
+    return F.interpolate(img.unsqueeze(0), scale_factor=(scale,scale)).squeeze(0)
 
 class Images(Dataset):
     def __init__(self, img_dir, ends='.png'):
@@ -13,4 +18,5 @@ class Images(Dataset):
     def __getitem__(self, index):
         img_path = self.images[index]
         img = torchvision.io.read_image(img_path)
-        return img, img_path
+        resized_img = resize_img(img)
+        return img, resized_img, img_path, index
