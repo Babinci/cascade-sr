471072bb1a877aeb37fd70c68d18d1597ebfd3a7
Babinci <walkowo@gmail.com>
2021-09-17 15:07:32 +0200

    added main function

--- train.py
+++ train.py
@@ -6,44 +6,45 @@ from dataset import *
 from FSRCNN import *
 from utils import *
 
+def main():
+    dataset = Images("../datasets", scale=8)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 
-dataset = Images("../datasets", scale=8)
-dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    lr_cur = 1e-3  # current lr
 
-lr_cur = 1e-3  # current lr
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    base_model = FSRCNN().to(device)
+   
+    criterion = nn.L1Loss()
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = FSRCNN().to(device)
-# model2 = Cascade()
+    optimizer = optim.Adam(
+        [
+            {"params": base_model.first_part.parameters()},
+            {"params": base_model.mid_part.parameters()},
+            {"params": base_model.last_part.parameters(), "lr": lr_cur * 0.1},
+        ],
+        lr=lr_cur,
+    )
+    model2 = Cascade2(base_model)
+    # optimizer = optim.Adam(lr=1e-4)
+    epochs = 1000
 
-# criterion = nn.MSELoss()
-criterion = nn.L1Loss()
+    for epoch in tqdm(range(epochs)):
+        for img, resized_img2, resized_img4, resized_img8, _, _ in dataloader:
 
-optimizer = optim.Adam(
-    [
-        {"params": model.first_part.parameters()},
-        {"params": model.mid_part.parameters()},
-        {"params": model.last_part.parameters(), "lr": lr_cur * 0.1},
-    ],
-    lr=lr_cur,
-)
-model2 = Cascade2(model)
-# optimizer = optim.Adam(lr=1e-4)
-epochs = 100
+            pred0, pred1, pred2 = model2(resized_img8)
 
-for epoch in tqdm(range(epochs)):
-    for img, resized_img2, resized_img4, resized_img8, _, _ in dataloader:
+            # W, H = img.shape[-1], img.shape[-2]
 
-        pred0, pred1, pred2 = model2(resized_img8)
+            L0 = criterion(pred0, resized_img4)#/ ((0.25**2) *W*H)
+            L1 = criterion(pred1, resized_img2)# / ((0.5**2) *W*H)
+            L2 = criterion(pred2, img)# / (W*H)
+            loss = L0 + L1 + L2
 
-        L0 = criterion(pred0, resized_img4)
-        L1 = criterion(pred1, resized_img2)
-        L2 = criterion(pred2, img)
-        loss = L0 + L1 + L2
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    print(loss)
-model2._save_to_state_dict("model1.pth")
+        print(loss)
+if __name__ == "__main__":
+    main()
