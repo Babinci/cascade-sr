09c75750909c3280edc05d45777efe988c79fa9e
Wojtek <walkowo@gmail.com>
2022-01-25 23:05:00 +0100

    added validation phase and mean loss for tensorboard

--- train.py
+++ train.py
@@ -10,10 +10,13 @@ from torch.utils.tensorboard import SummaryWriter
 
 writer = SummaryWriter()
 
+torch.manual_seed(0)
+
 def main():
-    dataset = Images("../datasets/train/", scale=8)
-    print(len(dataset))
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    init_dataset = Images("../datasets/train/", scale=8)
+    train_dataset, val_dataset = torch.utils.data.dataset.random_split(init_dataset, [172, 19])
+    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=False)
+    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False)
     lr_cur = 1e-3  # current lr
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -29,16 +32,19 @@ def main():
         ],
         lr=lr_cur,
     )
-    model2 = Cascade(base_model)
+    model = Cascade(base_model)
     
-    epochs = 1000
+    epochs = 250
+
+    i = 0
 
     for epoch in tqdm(range(epochs)):
-        for img, resized_img2, resized_img4, resized_img8, _, _ in dataloader:
+        #train loop
+        model.train()
+        for img, resized_img2, resized_img4, resized_img8, _, _ in train_dataloader:
 
-            pred0, pred1, pred2 = model2(resized_img8.to(device))
+            pred0, pred1, pred2 = model(resized_img8.to(device))
 
-            
             L0 = criterion(pred0, resized_img4.to(device))
             L1 = criterion(pred1, resized_img2.to(device))
             L2 = criterion(pred2, img.to(device))
@@ -48,12 +54,31 @@ def main():
             loss.backward()
             optimizer.step()
 
-    #optimizer updating
-    for g in optimizer.param_groups:
-        g['lr'] = g['lr'] * (0.1 ** int( epoch / (epochs * 0.8) ) )
-    
+        #validation
+        model.eval()
+        with torch.no_grad():
+            val_loss = []
+            for img, resized_img2, resized_img4, resized_img8, _, _ in val_dataloader:
 
+                pred0, pred1, pred2 = model(resized_img8.to(device))
 
-    writer.add_scalar('Loss/train', loss, epoch)
+                L0 = criterion(pred0, resized_img4.to(device))
+                L1 = criterion(pred1, resized_img2.to(device))
+                L2 = criterion(pred2, img.to(device))
+
+                loss = L0 + L1 + L2
+                val_loss.append(loss)
+            avg_val_loss = torch.stack(val_loss).mean()
+            writer.add_scalar('val_loss', avg_val_loss, epoch)
+
+        #optimizer updating as in paper
+        for g in optimizer.param_groups:
+            g['lr'] = g['lr'] * (0.1 ** int( epoch / (epochs * 0.8) ) )
+        
+        # path = f'../model_chkpoints/{i}.pth'
+        # torch.save(model2.state_dict(), path)
+        # i +=1 
+        
+
 if __name__ == "__main__":
     main()
